da696c126c1ee773e4825823d783fcf98506c1b9
vaibhav76276 <165167132+vaibhav76276@users.noreply.github.com>
2025-04-16 22:05:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,74 +3,75 @@ import threading
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
-class ChatServer:
-    def __init__(self, port=12345):
-        self.clients = {}
-        self.key = b'0123456789abcdef0123456789abcdef'
-        self.iv = b'abcdef9876543210'
+# Globals
+clients = {}
+key = b'0123456789abcdef0123456789abcdef'
+iv = b'abcdef9876543210'
 
-        self.host = socket.gethostbyname(socket.gethostname())  # 🟢 Dynamic IP
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, port))
-        self.server.listen()
-        print(f"Server started on {self.host}:{port}")
+host = socket.gethostbyname(socket.gethostname())
+port = 12345
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 
-    def encrypt(self, message):
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
-        encryptor = cipher.encryptor()
-        padded = message + b' ' * (16 - len(message) % 16)
-        return encryptor.update(padded) + encryptor.finalize()
+print(f"Server started on {host}:{port}")
 
-    def decrypt(self, data):
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
-        decryptor = cipher.decryptor()
-        return decryptor.update(data) + decryptor.finalize()
+def encrypt(message):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padded = message + b' ' * (16 - len(message) % 16)
+    return encryptor.update(padded) + encryptor.finalize()
 
-    def broadcast(self, message, exclude=None):
-        for client in self.clients:
-            if client != exclude:
-                try:
-                    client.send(message.encode())
-                except:
-                    self.remove_client(client)
+def decrypt(data):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    return decryptor.update(data) + decryptor.finalize()
 
-    def remove_client(self, client):
-        if client in self.clients:
-            username = self.clients[client]
-            print(f"{username} disconnected.")
-            self.broadcast(f"{username} left the chat.")
-            client.close()
-            del self.clients[client]
+def broadcast(message, exclude=None):
+    for client in clients:
+        if client != exclude:
+            try:
+                client.send(message.encode())
+            except:
+                remove_client(client)
 
-    def handle_client(self, client):
-        try:
-            encrypted = client.recv(1024)
-            decrypted = self.decrypt(encrypted).decode('utf-8').rstrip()
-            if decrypted.startswith("USERNAME:"):
-                username = decrypted.split(":", 1)[1]
-                self.clients[client] = username
-                self.broadcast(f"{username} joined the chat.")
-                print(f"{username} connected.")
-            else:
-                return
+def remove_client(client):
+    if client in clients:
+        username = clients[client]
+        print(f"{username} disconnected.")
+        broadcast(f"{username} left the chat.")
+        client.close()
+        del clients[client]
 
-            while True:
-                encrypted_msg = client.recv(1024)
-                if not encrypted_msg:
-                    break
-                decrypted = self.decrypt(encrypted_msg).decode('utf-8').rstrip()
-                print(f"[Encrypted]: {encrypted_msg}")
-                print(f"[Decrypted]: {decrypted}")
-                self.broadcast(decrypted, exclude=None)
-        except Exception as e:
-            print("Error:", e)
-        finally:
-            self.remove_client(client)
+def handle_client(client):
+    try:
+        encrypted = client.recv(1024)
+        decrypted = decrypt(encrypted).decode().rstrip()
+        if decrypted.startswith("USERNAME:"):
+            username = decrypted.split(":", 1)[1]
+            clients[client] = username
+            broadcast(f"{username} joined the chat.")
+            print(f"{username} connected.")
+        else:
+            return
 
-    def start(self):
         while True:
-            client, addr = self.server.accept()
-            threading.Thread(target=self.handle_client, args=(client,), daemon=True).start()
+            encrypted_msg = client.recv(1024)
+            if not encrypted_msg:
+                break
+            decrypted = decrypt(encrypted_msg).decode().rstrip()
+            print(f"[Encrypted]: {encrypted_msg}")
+            print(f"[Decrypted]: {decrypted}")
+            broadcast(decrypted)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        remove_client(client)
 
-if __name__ == "__main__":
-    ChatServer().start()
+def start_server():
+    while True:
+        client, addr = server.accept()
+        threading.Thread(target=handle_client, args=(client,), daemon=True).start()
+
+# Start the server
+start_server()
